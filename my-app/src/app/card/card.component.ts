346086d1bf6f35d52174f52e2a7aa65a4c08a457
margarita-sh <margaritka1992@gmail.com>
2020-10-14 03:02:02 +0300

    add img

--- my-app/src/app/card/card.component.ts
+++ my-app/src/app/card/card.component.ts
@@ -10,11 +10,11 @@ export class CardComponent implements OnInit {
  @Input() public movie: Search;
 /*   constructor() { } */
 public imageBaseurl: string = 'https://image.tmdb.org/t/p/w500';
-// public srcMovie: string = this.imageBaseurl + this.movie.poster_path;
+/* public srcMovie: string = this.imageBaseurl + this.movie.poster_path; */
 
   public ngOnInit(): void {
-	 console.log('movie', this.movie);
-	 console.log('srcMovie', this.srcMovie)
-	console.log('poster_path', this.movie.poster_path); */
+	console.log('movie', this.movie);
+/* 	console.log(this.srcMovie); */
+	console.log('poster_path', this.movie.poster_path);
   }
 }
