6086e32aeef84a99b9db6c05a36ef61df9cbae8a
margarita-sh <margaritka1992@gmail.com>
2020-11-16 14:53:16 +0300

    add form on input

--- my-app/src/app/components/search/search.component.ts
+++ my-app/src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
 	selector: 'app-search',
@@ -6,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-	public nameMovie: string;
-
+/* 	public nameMovie: string; */
+public nameMovie: string;
+constructor(public router: Router) {
+}
 	// tslint:disable-next-line: no-empty
 	public ngOnInit(): void {
+	}
+
+	/* public onSubmit(event: any): void {
+		console.log('yyy', this.nameMovie);
+		event.preventDefault();
+		this.router.navigateByUrl('/search/' + this.nameMovie);
+
+	} */
+
+	public submit(form: NgForm): void {
+		console.log(form);
+		this.router.navigateByUrl('/search/' + this.nameMovie);
 	}
 }
